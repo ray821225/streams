0ceeea7f70b827646334f5b25a92d1e629482c47
ray821225 <h24172037@gmail.com>
2021-08-22 22:32:01 +0800

    add main actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,14 @@
+import { formValues } from "redux-form";
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
 
 export const signIn = (userId) => {
   return {
@@ -15,6 +24,46 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues) => async (dispatch, getStore) => {
-  console.log("innnnn~~~");
-  streams.post("/streams", formValues);
+  const response = await streams.post("/streams", formValues);
+
+  dispatch({
+    action: "CREATE_STREAM",
+    payload: response.data,
+  });
+};
+
+export const fetchStreams = () => async (dispatch, getStore) => {
+  const response = await streams.get("/streams");
+
+  dispatch({
+    action: "FETCH_STREAMS",
+    paylolad: response.data,
+  });
+};
+
+export const fetchStream = (id) => async (dispatch, getStore) => {
+  const response = await streams.get(`/stream/${id}`);
+
+  dispatch({
+    action: "FETCH_STREAM",
+    payload: response.data,
+  });
+};
+
+export const editStream = (id, formValues) => async (dispatch, getStore) => {
+  const response = await streams.put(`/stream/${id}`);
+
+  dispatch({
+    action: "UPDATE_STREAM",
+    payload: response.data,
+  });
+};
+
+export const deleteStream = (id) => async (dispatch, getStore) => {
+  await streams.delete(`/stream/${id}`);
+
+  dispatch({
+    action: "DELETE_STREAM",
+    payload: id,
+  });
 };
