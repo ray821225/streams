08707178eea638ff34a8bdef2b82c5529fd306b9
ray821225 <h24172037@gmail.com>
2021-10-10 22:27:55 +0800

    add function component(StreamList) :wq

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,16 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
+import streams from "../../apis/streams";
 
-//class 寫法
-class StreamList extends React.Component {
-  componentDidMount() {
-    this.props.fetchStreams();
-  }
+//class  寫法  ------start-----
+// class StreamList extends React.Component {
+//   componentDidMount() {
+//     this.props.fetchStreams();
+//   }
 
-  renderAdmin(stream) {
-    if (stream.userId === this.props.currentUserId) {
+//   renderAdmin(stream) {
+//     if (stream.userId === this.props.currentUserId) {
+//       return (
+//         <div className="right floated content">
+//           <button className="ui button primary">Edit</button>
+//           <button className="ui button negative">Delete</button>
+//         </div>
+//       );
+//     }
+//   }
+
+//   renderList() {
+//     return this.props.streams.map((stream) => {
+//       return (
+//         <div className="item" key={stream.id}>
+//           {this.renderAdmin(stream)}
+//           <i className="large middle aligned icon camera" />
+//           <div className="content">
+//             {stream.title}
+//             <div className="description">{stream.description}</div>
+//           </div>
+//         </div>
+//       );
+//     });
+//   }
+
+//   renderCreate() {
+//     if (this.props.isSignedIn) {
+//       return (
+//         <div style={{ textAlign: "right" }}>
+//           <Link to="/streams/new" className="ui button primary">
+//             Creatr Stream
+//           </Link>
+//         </div>
+//       );
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h2></h2>
+//         <div className="ui celled list">{this.renderList()}</div>
+//         {this.renderCreate()}
+//       </div>
+//     );
+//   }
+// }
+
+//class  寫法  ------end-----
+
+//function component -----start-----
+
+const StreamList = ({ streams, currentUserId, isSignedIn, fetchStreams }) => {
+  useEffect(() => {
+    fetchStreams();
+  }, []);
+
+  const renderAdmin = (stream) => {
+    if (stream.userId === currentUserId) {
       return (
         <div className="right floated content">
           <button className="ui button primary">Edit</button>
@@ -18,13 +77,13 @@ class StreamList extends React.Component {
         </div>
       );
     }
-  }
+  };
 
-  renderList() {
-    return this.props.streams.map((stream) => {
+  const renderList = (streams) => {
+    return streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
-          {this.renderAdmin(stream)}
+          {renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -33,10 +92,10 @@ class StreamList extends React.Component {
         </div>
       );
     });
-  }
+  };
 
-  renderCreate() {
-    if (this.props.isSignedIn) {
+  const renderCreate = (isSignedIn) => {
+    if (isSignedIn) {
       return (
         <div style={{ textAlign: "right" }}>
           <Link to="/streams/new" className="ui button primary">
@@ -45,22 +104,18 @@ class StreamList extends React.Component {
         </div>
       );
     }
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <h2></h2>
-        <div className="ui celled list">{this.renderList()}</div>
-        {this.renderCreate()}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h2></h2>
+      <div className="ui celled list">{renderList(streams)}</div>
+      {renderCreate(isSignedIn)}
+    </div>
+  );
+};
 
-// const StreamList = () => {
-//   return <div>StreamList</div>;
-// };
+//function component -----end-----
 
 const mapStateToProps = (state) => {
   return {
